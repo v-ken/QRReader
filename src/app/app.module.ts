787d4ab1fa847906f446e9c6a06b1b3219087f4d
V-Ken Chin <ken.c@applickable.io>
2018-07-16 10:00:19 +1000

    Integrate QR scanner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
@@ -10,6 +11,8 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { QRScanner } from '@ionic-native/qr-scanner';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
 
 @NgModule({
   declarations: [
@@ -21,6 +24,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
@@ -34,7 +38,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    QRScanner,
+    BarcodeScanner
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  private history: string[] = [];
+  private currentResult: string = "N/A";
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(
+    public navCtrl: NavController,
+    private ref: ChangeDetectorRef,
+    private qrScanner: QRScanner,
+    private httpClient: HttpClient) {
   }
 
+  private scanAction() {
+    this.startScanning();
+    this.showCamera();
+  }
+
+  private startScanning() {
+    // Start a scan. Scanning will continue until something is detected or
+    // `QRScanner.cancelScan()` is called.
+    this.currentResult = "Scanning...";
+    this.qrScanner.scan().subscribe(
+      (text) => {
+        if (this.history.indexOf(text) == -1) {
+          console.log(text);
+          this.history.push(text);
+          this.currentResult = text;
+          this.ref.detectChanges();
+          this.httpClient.get(text).subscribe(
+            res => console.log(res)
+          );
+        }
+        // Continuously scan
+        this.startScanning();
+      }
+    )
+
+    // Make the webview transparent so the video preview is visible behind it.
+    this.qrScanner.show();
+    // Be sure to make any opaque HTML elements transparent here to avoid
+    // covering the video.
+  }
+
+  private showCamera() {
+    // Make background transparent to show the video feed
+   (window.document.querySelector('ion-app') as HTMLElement).classList.add('cameraView');
+  }
+
+  private hideCamera() {
+   (window.document.querySelector('ion-app') as HTMLElement).classList.remove('cameraView');
+   let content = <HTMLElement>document.getElementsByTagName("body")[0];
+   content.style.background = "white !important";
+  }
 }
